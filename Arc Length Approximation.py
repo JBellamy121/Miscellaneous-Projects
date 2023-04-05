2879f649319ad45a64839fd4b465629d2853d3b2
Jack Bellamy <127173121+JBellamy121@users.noreply.github.com>
2023-04-05 23:12:20 +1000

    Update Arc Length Approximation.py

--- Arc Length Approximation.py
+++ Arc Length Approximation.py
@@ -1,49 +1,74 @@
 # -*- coding: utf-8 -*-
 """
 Author: Jack Bellamy
-Date: 3/04/23
+Date: 5/04/23
 Task: Arc Length Approximation
 """
 
 #Modules Used
 import math
 import matplotlib.pyplot as plt
+import time
+import scipy.integrate as integrate
 
 
-def arcApprox():
+
+#Comparison of Methods used to generate arc length of a function
+def approxComparison():
     
-    sampleMax = 100000
-    
-    arcLengths = []
-    for i in range(2,sampleMax):
-        
-        length = arcLength(i)
-        arcLengths.append(length)
-    
-    # print(arcLengths)
-    arcDifference = []
+    firstTime = []
+    secondTime = []
+    firstResult = []
+    secondResult = []
     xAxis = []
-    finalLength = arcLengths[len(arcLengths)-1]
     
-    for j in range(0,len(arcLengths)):
+    intervals = [10*2**i for i in range(1,17)] #Generate logarithmically spaced points
+    for i in range(0,len(intervals)):
         
-        diff = abs(finalLength - arcLengths[j]) / finalLength
-        arcDifference.append(diff)
-        xAxis.append(j)
-    
-    #Plot of percentage error
-    plt.plot(xAxis,arcDifference)
+        sampleSize = intervals[i] #Choose sample amount from list
+        
+        #Statisitics for first method: arcLengthDistance()
+        timeFirst = time.time()
+        resultFirst = arcLengthDistance(sampleSize)
+        timeFirst = time.time() - timeFirst
+        #Statisitics for second method: arcLengthIntegral()
+        timeSecond = time.time()
+        resultSecond = arcLengthIntegral(sampleSize)
+        timeSecond = time.time() - timeSecond
+        #Appending results to respective lists
+        firstTime.append(timeFirst)
+        secondTime.append(timeSecond)  
+        firstResult.append(resultFirst)
+        secondResult.append(resultSecond)
+        xAxis.append(sampleSize)
+
+    #Plotting for time results of two methods
+    plt.plot(xAxis,firstTime,c='blue',label='Distance Method')
+    plt.plot(xAxis,secondTime,c='red',label='Integral Method')
+    plt.title('Times of First Method')
     plt.grid()
-    plt.title("Percentage Error as sample amount approachs 'sampleMax'")
-    
-    print('Final Arc Length Approximation: ',str(round(finalLength,6)))
-    
-    
+    plt.legend()
+    plt.show()
+    plt.clf()
+
+    #Plotting for arc length result of two methods
+    plt.plot(xAxis,firstResult,c='blue',label='Distance Method')
+    plt.plot(xAxis,secondResult,c='red',label='Integral Method')
+    plt.title('Results of First Method')
+    plt.grid()
+    plt.legend()
+    plt.show()
+
+
+##########################################################################################
+##########################################################################################
+##########################################################################################
+
 #Function calculates Arc Length of curve defined in function 'fx()'
-def arcLength(samples):
+def arcLengthDistance(samples):
     
-    xA = -1 #Initial point of arc 
-    xB = 1 #End point of arc 
+    xA = 0 #Initial point of arc 
+    xB = 100 #End point of arc 
     xDelta = abs(xB - xA) / samples #Step size between samples
     
     points = [] #List of sample points
@@ -57,24 +82,67 @@ def arcLength(samples):
     arcLength = 0 #Arc length variable used in loop below
     for i in range(0,len(points) - 1):
         
-        arcLength += dist(points[i],points[i+1]) #Adding distances between samples to calculate Arc Length   
+        arcLength += math.dist(points[i],points[i+1]) #Adding distances between samples to calculate Arc Length   
     
     # print(arcLength) #Prints the arc Length estimated to user
     return arcLength
+
+
+###################
+
+#Function calculates arc length using numerical integration.
+def arcLengthIntegral(samples):
+
+    x = 0 #Initial point of arc 
+    xB = 100 #End point of arc 
+    xDelta = abs(xB - x) / samples #Step size between samples
     
+    yValues = []
+    while x < xB:
+
+        y = math.sqrt(1 + math.pow(dfx(x),2))       
+        yValues.append(y)
+        x += xDelta
+        
+    areaSum = 0
+    for i in range(1,len(yValues) - 1):
+        areaSum += 2 * yValues[i]
+         
+    area = 0.5 * xDelta * (yValues[0] + yValues[len(yValues)-1] + areaSum)
+    return area 
+
+
+#Function calculates arc length using numerical integration.
+def arcLengthIntegral2():
+
+    xA = 0 #Initial point of arc 
+    xB = 100 #End point of arc 
+
+    integral = integrate.quad(arcFunc,xA,xB)
+    
+    print(integral[0])
+    
+#Formula for arc length
+def arcFunc(x):   
+    
+    L = math.sqrt(1 + math.pow(dfx(x),2))
+    return L
+
+##########################################################################################
+##########################################################################################
+##########################################################################################
 
 #The curve that will have its arc length approximated
 def fx(x):
     
-    y = math.sqrt(1 - math.pow(x,2)) #The curve/function 
+    y = math.sqrt(15*x) * ((math.pow(x,2)*math.cos(x))/math.exp(x))
     return y
 
-
-#Calculates distance between point 'p1' and 'p2'
-def dist(p1,p2):
+#Derivative of fx()
+def dfx(x):
     
-    x0 = p1[0] ; y0 = p1[1]
-    x1 = p2[0] ; y1 = p2[1]
-    
-    distance = math.sqrt((math.pow(x1 - x0,2) + math.pow(y1 - y0,2)))
-    return distance
+    dy1 = -0.5 
+    dy2 = ((math.sqrt(15)*(x**3/2)*math.exp(-x)))  
+    dy3 = ((2*x*math.sin(x)) + ((2*x - 5)*(math.cos(x))))
+    dy = dy1 * (dy2 * dy3)
+    return dy
